4f82f4b89035a6927358e8ca4d6b25f0ef30bd62
PiotrGrzyb <piotrg6711@gmail.com>
2021-06-02 09:16:13 +0200

    Test file

--- testclass/src/src/klasa1.java
+++ testclass/src/src/klasa1.java
@@ -1,3 +1,58 @@
 package src;
+
+import java.util.List;
+
+
 public class klasa1 {
+int licznik;
+
 }
+
+class klasa2 extends klasa1 {
+	
+}
+
+class klasa3 extends klasa2{
+	
+}
+
+class klasa4 extends klasa1 implements fejs{
+	
+}
+
+class klasa5 extends klasa1{
+}
+
+
+interface fejs{
+	
+}
+
+class Book {
+	 
+    public String title;
+    public String author;
+ 
+    Book(String title, String author)
+    {
+ 
+        this.title = title;
+        this.author = author;
+    }
+}
+
+class Library {
+	 
+    // reference to refer to list of books.
+    private final List<Book> books;
+ 
+    Library(List<Book> books) { this.books = books; }
+ 
+    // Getting total number of books
+    public List<Book> getTotalBooksInLibrary()
+    {
+ 
+        return books;
+    }
+}
+ 
